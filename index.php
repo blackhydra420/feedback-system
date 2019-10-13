0dce52ab5821c6a1befcf0d68383820307b3e2f1
Anand Sharma <anandsharma110397@gmail.com>
2019-10-13 16:31:43 +0530

    login page edited

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,17 +9,62 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/cdf493be5b.js"></script>
     <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
 </head>
+
+<style>
+    body {
+        font-family: 'Open Sans', 
+                     'Lucida Sans', 
+                     'Lucida Sans Regular', 
+                     'Lucida Grande', 
+                     'Lucida Sans Unicode', 
+                     Geneva, 
+                     Verdana, 
+                     sans-serif !important;
+    }
+
+    .login-bg {
+        height: 100vh;
+        background-color: #1fc8db;
+        background-image: url('assets/images/john.jpg');
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+    }
+</style>
+
 <body>
     <?php
-        include_once 'include/navbar.php';
+    //    include_once 'include/navbar.php';
     ?>
-    <div class="container main-body shadow-lg">
-    
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 login-bg">
+                
+            </div>
+            <div class="col-md-6">
+                <form method="" action="" class="w-75" style="margin-top: 20%!important; margin: auto auto;">
+                    <h1 class="text-center mb-5 font-weight-bold"> Feedback login </h1>
+                    <div class="form-group">
+                        <label for="username">Enrollment Number</label>
+                        <input type="text" class="form-control" id="username" placeholder="0302XXXXX001">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="**********">
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-dark px-5 rounded-pill">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
     <?php
-        include_once 'include/footer.php';
+    // include_once 'include/footer.php';
     ?>
 </body>
+
 </html>
